c4ecb1f6194d178d32e76637660da6a1962f7915
e5021440 <akashadi@titan.co.in>
2025-09-02 10:46:54 +0530

    Code standzations

--- predictive/serializer.py
+++ predictive/serializer.py
@@ -1,25 +1,38 @@
+# Import the necessary module from Django REST Framework
 from rest_framework import serializers
-from .models import *
 
+# Import all models from the current module
+from .models import *
+# from .models import SensorDataLog, ErrorLog, SettingsElement, AnomalyDataLog
+
+
+# Serializer for the SensorDataLog model
 class SensorDataLogSerializer(serializers.ModelSerializer):
     class Meta:
         model = SensorDataLog
+        # Exclude the 'id' field from serialization
         exclude = ['id']
 
 
+# Serializer for the ErrorLog model
 class ErrorLogSerializer(serializers.ModelSerializer):
     class Meta:
         model = ErrorLog
+        # Exclude the 'id' field from serialization
         exclude = ['id']
 
 
+# Serializer for the SettingsElement model
 class SettingsElementSerializer(serializers.ModelSerializer):
     class Meta:
         model = SettingsElement
-        fields = '__all_'
+        # Fix the typo here: '__all_' → '__all__'
+        fields = '__all__'  # Include all fields in the model
 
+
+# Serializer for the AnomalyDataLog model
 class AnomalyDataLogSerializer(serializers.ModelSerializer):
     class Meta:
         model = AnomalyDataLog
+        # Exclude the 'id' field from serialization
         exclude = ['id']
-
--- predictive/urls.py
+++ predictive/urls.py
@@ -1,62 +1,60 @@
-from django.contrib import admin
-from django.urls import path, include
-from predictive import views as pred
+from django.urls import path
+from predictive import views as pdm
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('', pred.login_screen, name='login_screen'),
+    path('', pdm.login_screen, name='login_screen'),
+    path('dashboard/', pdm.dashboard, name='dashboard'),
+    path('get_plants/', pdm.get_plants, name='get_plants'),
+    path('get_lines/', pdm.get_lines, name='get_lines'),
 
-    path('dashboard/', pred.dashboard, name='dashboard'),
-    path('get_plants/', pred.get_plants, name='get_plants'),
-    path('get_lines/', pred.get_lines, name='get_lines'),
+    path('training_screen/', pdm.training_screen, name='training_screen'),
 
-    path('training_screen/', pred.training_screen, name='training_screen'),
+    path('model_analysis/', pdm.model_analysis, name='model_analysis'),
+    path('get_models/', pdm.get_models, name='get_models'),
+    path('model_analysis_chart/', pdm.model_evaluation, name='model_evaluation'),
 
-    path('model_analysis/', pred.model_analysis, name='model_analysis'),
-    path('get_models/', pred.get_models, name='get_models'),
-    path('model_analysis_chart/', pred.model_evaluation, name='model_evaluation'),
+    path('organization_create', pdm.organization_create, name='organization_create'),
+    path('organization_list/', pdm.organization_list, name='organization_list'),
+    path('org_update/<int:id>/', pdm.update_org, name='org_update'),
+    path('organization_org/<int:id>/', pdm.organization_org, name='organization_org'),
 
-    path('organization_create', pred.organization_create, name='organization_create'),
-    path('organization_list/', pred.organization_list, name='organization_list'),
-    path('org_update/<int:id>/', pred.update_org, name='org_update'),
-    path('organization_org/<int:id>/', pred.organization_org, name='organization_org'),
+    path('summary_list/', pdm.summary_list, name='summary_list'),
 
-    path('summary_list/', pred.summary_list, name='summary_list'),
+    path('email_list/', pdm.email_list, name='email_list'),
+    path('email_create/', pdm.email_create, name='email_create'),
+    path('email_update/<int:id>/', pdm.email_update, name='email_update'),
+    path('email_delete/<int:id>/', pdm.email_delete, name='email_delete'),
 
-    path('email_list/', pred.email_list, name='email_list'),
-    path('email_create/', pred.email_create, name='email_create'),
-    path('email_update/<int:id>/', pred.email_update, name='email_update'),
-    path('email_delete/<int:id>/', pred.email_delete, name='email_delete'),
+    path('sensor_export/', pdm.sensor_export,name='sensor_export'),
+    path('sensor_import/', pdm.sensor_import,name='sensor_import'),
+    path('sensor_list/', pdm.sensor_list, name='sensor_list'),
+    path('sensor_create', pdm.sensors_create, name='sensor_create'),
+    path('sensor_update/<int:id>/', pdm.update_sensor, name='sensor_update'),
+    path('sensor_delete/<int:id>/', pdm.delete_sensor, name='sensor_delete'),
 
-    path('sensor_export/', pred.sensor_export,name='sensor_export'),
-    path('sensor_import/', pred.sensor_import,name='sensor_import'),
-    path('sensor_list/', pred.sensor_list, name='sensor_list'),
-    path('sensor_create', pred.sensors_create, name='sensor_create'),
-    path('sensor_update/<int:id>/', pred.update_sensor, name='sensor_update'),
-    path('sensor_delete/<int:id>/', pred.delete_sensor, name='sensor_delete'),
+    path('anomaly/', pdm.anomaly_view, name='anomaly_view'),
+    path('get_startdate/', pdm.get_startdate, name='get_startdate'),
 
-    path('anomaly/', pred.anomaly_view, name='anomaly_view'),
+    path('predictive_screen/', pdm.predictive_screen, name='predictive_screen'),
+    path('run_predictions/', pdm.run_predictions, name='ajax_predictive_screen-data'),
+    path('run_predictions1/', pdm.run_predictionsAkash, name='ajax_predictive'),
 
-    path('get_startdate/', pred.get_startdate, name='get_startdate'),
+    path('check_server/', pdm.is_server_live),
+    path('datalog/', pdm.datalog),
+    path('pred_sensors/', pdm.get_pred_sensor_list),
+    path('sensordata/', pdm.get_sensor_data),
+    path('datalog_sensor/', pdm.datalog_sensor_list),
+    path('error_log/', pdm.error_log),
+    path('test/', pdm.test_function),
+    path('train_model/', pdm.train_model, name='train_model'),
+    path('data/', pdm.element_raw_data, name='ajax_sensor_data'),
+    path('predict/', pdm.run_predictions),
+    path('delete_all/<str:sensor_id>', pdm.delete_all_records),
+    path('anamoly_records/', pdm.refresh_anomalies),
+    path('alert/', pdm.email_anamoly_alert),
+    path('exception_job/',pdm.exception_job),
 
-    path('predictive_screen/', pred.predictive_screen, name='predictive_screen'),
-    path('run_predictions/', pred.run_predictions, name='ajax_predictive_screen-data'),
-    path('run_predictions1/', pred.run_predictionsAkash, name='ajax_predictive'),
-
-    path('check_server/', pred.is_server_live),
-    path('datalog/', pred.datalog),
-    path('pred_sensors/', pred.get_pred_sensor_list),
-    path('sensordata/', pred.get_sensor_data),
-    path('datalog_sensor/', pred.datalog_sensor_list),
-    path('error_log/', pred.error_log),
-    path('test/', pred.test_function),
-    path('train_model/', pred.train_model, name='train_model'),
-    path('data/', pred.element_raw_data, name='ajax_sensor_data'),
-    path('predict/', pred.run_predictions),
-    path('delete_all/<str:sensor_id>', pred.delete_all_records),
-    path('anamoly_records/', pred.refresh_anomalies),
-    path('alert/', pred.email_anamoly_alert),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('exception_job/',pred.exception_job),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
